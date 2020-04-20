d9b01808ae8448768b52c337a30db2024f95e644
Victor Espinoza <vespinoza@prokarma.com>
2020-04-20 16:18:58 -0600

    Main xlsx file reader

--- src/main/java/com/data/Constants.java
+++ src/main/java/com/data/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
 	public static final Constants BROWSER = new Constants("BROWSER");
 	public static final Constants ENVIRONMENT = new Constants("ENVIRONMENT");
-
+	public static final Constants DATA_FILE = new Constants("DATA_FILE");
 	
 	private String name;
 	
--- src/main/java/com/dummy/MainTest.java
+++ src/main/java/com/dummy/MainTest.java
@@ -1,12 +1,51 @@
 package com.dummy;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
+
 import com.data.Constants;
 import com.data.PropertyLoader;
 
 public class MainTest {
-	public static void main(String [] args) {
-		System.out.println(PropertyLoader.getProperty(Constants.BROWSER));
-		System.out.println(PropertyLoader.getProperty(Constants.ENVIRONMENT));
+	public static void main(String [] args) throws FileNotFoundException {
+		
+		Workbook wBook = null;
+		InputStream file = new FileInputStream(PropertyLoader.getProperty(Constants.DATA_FILE));
+		
+		try {
+			wBook = new XSSFWorkbook(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Sheet wSheet = wBook.getSheetAt(0);
+		
+		Row wRow = wSheet.getRow(0);
+	
+		Cell wCell = wRow.getCell(1);
+		
+		for (int i = 0; i <= wSheet.getLastRowNum(); i++) {
+			
+			wRow = wSheet.getRow(i);
+			
+			for (int j = 0; j <= wRow.getLastCellNum(); j++) {
+				wCell = wRow.getCell(j);
+				System.out.println(wCell);
+			}
+				
+		}
+		
+		System.out.println(wBook);
 		
 		
 	}
